9c9267e6ed87f2f18a558e4136800c016f598de4
ersalomo <95175703+ersalomo@users.noreply.github.com>
2022-08-24 16:16:58 +0700

    login and dashboard admin

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware(['guest:web'])->group(function () {
+    Route::view('/', 'auth/auth-login')->name('login');
+    Route::view('dashboard-page', 'dashboard/dashboard-page')->name('dashboard-page');
 });
