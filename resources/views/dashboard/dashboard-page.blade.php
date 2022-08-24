296b4962d2f340e4c7ac8301790a43792c33294c
ersalomo <95175703+ersalomo@users.noreply.github.com>
2022-08-24 17:19:34 +0700

    registrasi form

--- resources/views/dashboard/dashboard-page.blade.php
+++ resources/views/dashboard/dashboard-page.blade.php
@@ -1042,7 +1042,8 @@
                             <li class="nav-item dropdown header-profile">
                                 <a class="nav-link" href="javascript:void(0);" role="button"
                                     data-bs-toggle="dropdown">
-                                    <img src="images/user.jpg" width="56" alt="" />
+                                    <img src="{{ asset('support/images/avatar/logo-b7.jpeg') }}" width="56"
+                                        alt="" />
                                 </a>
                                 <div class="dropdown-menu dropdown-menu-end">
                                     <a href="app-profile.html" class="dropdown-item ai-icon">
@@ -1205,12 +1206,8 @@
                                 <a href="chart-chartjs.html">Chartjs</a>
                             </li>
                             <li>
-                                <a href="chart-chartist.html">Chartist</a>
+                                <a href="{{ route('registrasi') }}">Form Registrasi</a>
                             </li>
-                            <li>
-                                <a href="chart-sparkline.html">Sparkline</a>
-                            </li>
-                            <li><a href="chart-peity.html">Peity</a></li>
                         </ul>
                     </li>
                     <li>
@@ -1350,40 +1347,9 @@
                         </ul>
                     </li>
                 </ul>
-                <div class="side-bar-profile">
-                    <div class="d-flex align-items-center justify-content-between mb-3">
-                        <div class="side-bar-profile-img">
-                            <img src="images/user.jpg" alt="" />
-                        </div>
-                        <div class="profile-info1">
-                            <h4 class="fs-18 font-w500">Soeng Souy</h4>
-                            <span>example@mail.com</span>
-                        </div>
-                        <div class="profile-button">
-                            <i class="fas fa-caret-down scale5 text-light"></i>
-                        </div>
-                    </div>
-                    <div class="d-flex justify-content-between mb-2 progress-info">
-                        <span class="fs-12"><i class="fas fa-star text-orange me-2"></i>Task Progress</span>
-                        <span class="fs-12">20/45</span>
-                    </div>
-                    <div class="progress default-progress">
-                        <div class="progress-bar bg-gradientf progress-animated" style="width: 45%; height: 10px"
-                            role="progressbar">
-                            <span class="sr-only">45% Complete</span>
-                        </div>
-                    </div>
-                </div>
 
-                <div class="copyright">
-                    <p>
-                        <strong>Fillow Saas Admin</strong> © 2021 All Rights
-                        Reserved
-                    </p>
-                    <p class="fs-12">
-                        Made with <span class="heart"></span> by DexignLabs
-                    </p>
-                </div>
+
+
             </div>
         </div>
         <!--**********************************
