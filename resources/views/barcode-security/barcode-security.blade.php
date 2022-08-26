6520fda3aa558c044e833816f0ca1b82c0e2fe83
matt1412-arky <79315420+matt1412-arky@users.noreply.github.com>
2022-08-26 21:19:43 +0700

    delete scroll bar at barcode

--- resources/views/barcode-security/barcode-security.blade.php
+++ resources/views/barcode-security/barcode-security.blade.php
@@ -8,67 +8,60 @@
     <title>Barcode | Bintang Toe Joe</title>
     <link href="support/css/style.css" rel="stylesheet" />
 </head>
-<div class="container-fluid " style="background:#1E90FF;">
-    <div class="row">
-        <div class="col-auto mt-2">
-            <img class="rounded" src="{{ asset('support/images/avatar/login-b7.jpg') }}" width="200" height="100"
-                alt="">
-        </div>
-    </div>
-    <div class="row mt-5 justify-content-center">
-        <div class="col-xl-5">
-            <div class="card">
-                <div class="card-header">
-                    <h5 class="card-title">Visitor Registration</h5>
+    <body style="overflow: hidden;">
+        <div class="container-fluid" style="background:#1E90FF;">
+            <div class="row">
+                <div class="col-auto mt-2">
+                    <img class="rounded" src="{{ asset('support/images/avatar/login-b7.jpg') }}" width="200" height="100"
+                        alt="">
                 </div>
-                <div class="card-body">
-                    <p class="card-text">This is a wider card with supporting text and below as a natural lead-in to the
-                        additional content. This content is a little</p>
-                </div>
-                <img class="card-img-bottom bg-primary img-fluid"
-                    src="{{ asset('support/icons/barcode/QR_code1.png') }}" alt="">
-
             </div>
-        </div>
-        <div class="col-xl-5">
-            <div class="card">
-                <div class="card-header">
-                    <h5 class="card-title">GPS</h5>
-                </div>
-                <div class="card-body">
-                    <p class="card-text">This is a wider card with supporting text and below as a natural lead-in to the
-                        additional content. This content is a little</p>
-                </div>
-                <img class="card-img-bottom img-fluid" src="{{ asset('support/icons/barcode/QR_code1.png') }}"
-                    alt="">
-            </div>
-        </div>
-
-
-
-        <div class="col-xl-10">
-            <div class="card">
-                <center>
-                    <div class="card-body ">
-                        <button type="button" class="btn btn-outline-primary btn-lg">
-                            Visitor Data
-                        </button>
-                        <button type="button" class="btn btn-outline-primary btn-lg">
-                            Make a temporary account
-                        </button>
-                        <button type="button" class="btn btn-outline-primary btn-lg">Track Visitor</button>
-                        <button type="button" class="btn btn-outline-primary btn-lg">Refresh</button>
+            <div class="row mt-5 justify-content-center">
+                <div class="col-xl-5">
+                    <div class="card">
+                        <div class="card-header">
+                            <h5 class="card-title">Visitor Registration</h5>
+                        </div>
+                        <div class="card-body">
+                            <p class="card-text">This is a wider card with supporting text and below as a natural lead-in to the
+                                additional content. This content is a little</p>
+                        </div>
+                        <img class="card-img-bottom bg-primary img-fluid"
+                            src="{{ asset('support/icons/barcode/QR_code1.png') }}" alt="">
+        
                     </div>
-                </center>
+                </div>
+                <div class="col-xl-5">
+                    <div class="card">
+                        <div class="card-header">
+                            <h5 class="card-title">GPS</h5>
+                        </div>
+                        <div class="card-body">
+                            <p class="card-text">This is a wider card with supporting text and below as a natural lead-in to the
+                                additional content. This content is a little</p>
+                        </div>
+                        <img class="card-img-bottom img-fluid" src="{{ asset('support/icons/barcode/QR_code1.png') }}"
+                            alt="">
+                    </div>
+                </div>
+        
+                <div class="col-xl-10">
+                    <div class="card">
+                        <center>
+                            <div class="card-body ">
+                                <button type="button" class="btn btn-outline-primary btn-lg">
+                                    Visitor Data
+                                </button>
+                                <button type="button" class="btn btn-outline-primary btn-lg">
+                                    Make a temporary account
+                                </button>
+                                <button type="button" class="btn btn-outline-primary btn-lg">Track Visitor</button>
+                                <button type="button" class="btn btn-outline-primary btn-lg">Refresh</button>
+                            </div>
+                        </center>
+                    </div>
+                </div>
             </div>
-        </div>
-
-
-
-    </div>
-
-    <body>
-
     </body>
 
 </html>
