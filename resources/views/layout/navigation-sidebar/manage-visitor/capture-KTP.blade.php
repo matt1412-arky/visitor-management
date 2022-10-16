6cd85df1dde923ea87493512bb864856341d865e
matt1412-arky <matt.cahyadi@yahoo.com>
2022-10-16 23:17:18 +0700

    updated

--- resources/views/layout/navigation-sidebar/manage-visitor/capture-KTP.blade.php
+++ resources/views/layout/navigation-sidebar/manage-visitor/capture-KTP.blade.php
@@ -2,14 +2,24 @@
 @section('title', 'Dashboard Admin')
 @section('content')
     {{-- berada pada conainer fluid --}}
-    <div class="card text-center">
+    <div class="card">
         <div class="card-header">
             <h5 class="card-title">Take capture KTP</h5>
         </div>
         <div class="card-body">
-
-            <p class="card-text">This is a wider card with supporting text and below as a natural lead-in to the additional
-                content. This content</p>
+            <div class="row">
+                <div class="mb-3 col-md-6 col-lg-12">
+                    <label class="form-label fs-4"><b class="text-red">*</b>NIK/No. SIM</label>
+                    <input type="text" class="form-control" placeholder="NIK/No. SIM" name="visitor">
+                </div>
+                <button type="submit" class="btn me-2 btn-google">Submit</button>
+                <div class="mt-3">
+                    <img src="{{ asset('support/images/cam.png') }}" class="rounded"
+                        style="width: 100%; height: 100%; margin: 0px;">
+                    {{-- <p class="card-text">This is a wider card with supporting text and below as a natural lead-in to the additional
+                content. This content</p> --}}
+                </div>
+            </div>
         </div>
         <center>
             <div class="col-xl-4 col-xxl-6 col-4">
--- resources/views/layout/navigation-sidebar/manage-visitor/form-kesehatan.blade.php
+++ resources/views/layout/navigation-sidebar/manage-visitor/form-kesehatan.blade.php
@@ -8,28 +8,28 @@
         </div>
         <div class="card-body">
             <div class="row">
-                <div class="col-xl-4 col-xxl-6 col-4">
+                <div class="col-xl-4 col-xxl-6 col-3">
                     <div class="form-check custom-checkbox mb-3 checkbox-success">
                         <input type="checkbox" class="form-check-input" id="customCheckBox1" required="">
                         <label class="form-check-label" for="customCheckBox1">Vaksin 1</label>
                     </div>
                 </div>
 
-                <div class="col-xl-4 col-xxl-6 col-4">
+                <div class="col-xl-4 col-xxl-6 col-3">
                     <div class="form-check custom-checkbox mb-3 checkbox-success">
                         <input type="checkbox" class="form-check-input" checked="" id="customCheckBox2" required="">
                         <label class="form-check-label" for="customCheckBox2">Vaksin 2</label>
                     </div>
                 </div>
 
-                <div class="col-xl-4 col-xxl-6 col-4">
+                <div class="col-xl-4 col-xxl-6 col-3">
                     <div class="form-check custom-checkbox mb-3 checkbox-success">
                         <input type="checkbox" class="form-check-input" checked="" id="customCheckBox3" required="">
                         <label class="form-check-label" for="customCheckBox3">Booster I</label>
                     </div>
                 </div>
 
-                <div class="col-xl-4 col-xxl-6 col-4">
+                <div class="col-xl-4 col-xxl-6 col-3">
                     <div class="form-check custom-checkbox mb-3 checkbox-success">
                         <input type="checkbox" class="form-check-input" checked="" id="customCheckBox3" required="">
                         <label class="form-check-label" for="customCheckBox3">Booster II</label>
@@ -39,15 +39,9 @@
                 <button type="submit" class="btn me-2 btn-google">Submit</button>
                 <br>
 
-<<<<<<< HEAD
                 <button type="button" class="btn me-2 btn-google">
-                    {{-- <a href="{{ route('home.capture-KTP') }}" style="color:white">Take capture KTP</a>  --}}
+                    <a href="{{ route('home.capture-KTP') }}" style="color:white">Take capture KTP</a>
                 </button>
-=======
-                <a href="{{ route('home.capture-ktp') }}" class="btn me-2 btn-google" style="color:white">Take capture
-                    KTP</a>
-
->>>>>>> 1e6fad9f8795c60ab7d80da319e5e19cc12e31fd
 
             </div>
         </div>
